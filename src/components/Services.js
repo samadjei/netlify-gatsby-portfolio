e64ccc7bbe93f0f7d2ad195105d01dd6f2b3de7e
Samuel Adjei <info.samadjei@gmail.com>
2021-02-06 22:39:59 +0000

    improved services content and changed site-wide padding

--- src/components/Services.js
+++ src/components/Services.js
@@ -9,13 +9,13 @@ import Launch from "../assets/svg/services/launch.svg"
 const Services = ({ title }) => {
   return (
     <section className="services">
-      <Title title={title} />
+      <Title title={title} className="title" />
       <div className="services__grid">
         <div className="services__content">
           <div className="icon">
             <img
-              width="150"
-              height="150"
+              width="200"
+              height="200"
               className="icon--size"
               src={Design}
               alt="Design"
@@ -24,17 +24,17 @@ const Services = ({ title }) => {
           <div className="services__details">
             <h3>Web Design</h3>
             <p>
-              I will provide you with a clean and modern design of what your
-              website could look like before development to set your
-              expectations.{" "}
+              I will use your feedback upon our conversation to provide you with
+              a modern design of what your website could look like. Different
+              iterations can be created until find the most suitable design.
             </p>
           </div>
         </div>
         <div className="services__content">
           <div className="icon">
             <img
-              width="150"
-              height="150"
+              width="200"
+              height="200"
               className="icon--size"
               src={Develop}
               alt="Design"
@@ -43,17 +43,17 @@ const Services = ({ title }) => {
           <div>
             <h3>Web Development</h3>
             <p>
-              I develop bespoke websites with the latest web tehcnologies to
-              ensure your site is modern, responsive and scalable to meet all
-              your business needs.
+              Up an agreed design, i will develop the website with the latest
+              web technologies to ensure your site is modern, responsive and
+              scalable to meet all your business requirements.
             </p>
           </div>
         </div>
         <div className="services__content">
           <div className="icon">
             <img
-              width="150"
-              height="150"
+              width="200"
+              height="200"
               className="icon--size"
               src={SEO}
               alt="Design"
@@ -62,17 +62,17 @@ const Services = ({ title }) => {
           <div>
             <h3>SEO</h3>
             <p>
-              Good SEO is an intergral part of any online business to allow a
-              site to generate organic traffic. I will ensure that your website
-              is SEO optimise to maxmise its potential.
+              Good SEO is an intergral part of any online business to generate
+              organic traffic. I will ensure that your website is SEO optimise
+              to maxmise its potential output to your future clients.
             </p>
           </div>
         </div>
         <div className="services__content">
           <div className="icon">
             <img
-              width="150"
-              height="150"
+              width="200"
+              height="200"
               className="icon--size"
               src={Launch}
               alt="Design"
@@ -81,9 +81,10 @@ const Services = ({ title }) => {
           <div>
             <h3>Content Management</h3>
             <p>
-              I will provide you with a clean and modern design of what your
-              website could look like before development to set your
-              expectations.{" "}
+              I will provide you with the means of managing the content of your
+              site to give you the ability to continuosly update your site. I
+              also offer a retainer package if you prefer not having to maintain
+              the site.
             </p>
           </div>
         </div>
