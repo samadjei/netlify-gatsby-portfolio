37f9a7dfac96cc7457fa186cdb811a203dcf5781
Samuel Adjei <info.samadjei@gmail.com>
2021-03-20 21:07:00 +0000

    Upadted content in 404 Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,12 +6,24 @@ import SEO from "../components/SEO"
 const Error = () => {
   return (
     <Layout>
-      <SEO title="Dead End" description="This page does not exist, please go back"/>
-      <main className="error-page">
-        <div className="error-container">
-          <h1>oops it's a dead end</h1>
+      <SEO
+        title="Dead End"
+        description="This page does not exist, please go back"
+      />
+      <main className="error container">
+        <div className="error__container">
+          <h1 className="error__header">
+            He! You've reached a dead end. This page does not exist
+          </h1>
+          <div className="conain">
+            <img
+              src="https://media.giphy.com/media/VwoJkTfZAUBSU/giphy.gif"
+              alt="404 GIF by Mathew Lucas"
+            />
+          </div>
+
           <Link to="/" className="btn">
-            back home
+            Go back home
           </Link>
         </div>
       </main>
